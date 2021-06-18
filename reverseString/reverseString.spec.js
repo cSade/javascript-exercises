7e325ade1d0dcd6d5ca5775a9766584265c7763f
cSade <83520367+cSade@users.noreply.github.com>
2021-06-17 19:46:23 -0500

    Update reverseString.spec.js

--- reverseString/reverseString.spec.js
+++ reverseString/reverseString.spec.js
@@ -5,14 +5,14 @@ describe('reverseString', () => {
     expect(reverseString('hello')).toEqual('olleh');
   });
 
-  test.skip('reverses multiple words', () => {
+  ('reverses multiple words', () => {
     expect(reverseString('hello there')).toEqual('ereht olleh')
   })
 
-  test.skip('works with numbers and punctuation', () => {
+  ('works with numbers and punctuation', () => {
     expect(reverseString('123! abc!')).toEqual('!cba !321')
   })
-  test.skip('works with blank strings', () => {
+  ('works with blank strings', () => {
     expect(reverseString('')).toEqual('')
   })
 });
