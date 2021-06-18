d8c85bda65182a658205e2ae25a1c01307dff44c
cSade <83520367+cSade@users.noreply.github.com>
2021-06-17 19:13:07 -0500

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,7 @@
 const repeatString = function() {
-
+  let secondString = repeatString;
+  for (var i = 0; i <= repeatString; i++);
+    return repeatString.repeat([secondString]);
 };
 
 module.exports = repeatString;
