2bf8a53c2e6c7fb12114dd1f7d9eea98d75b6adb
cSade <83520367+cSade@users.noreply.github.com>
2021-06-18 18:39:32 -0500

    Update sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,8 @@
 const sumAll = function(min, max) {
  return ((max-min)+1) * (min + max) / 2;
 }
+function finalSum(arr) {
+    return sumAll(Math.min(...arr), Math.max(...arr));
+}
 
 module.exports = sumAll;
