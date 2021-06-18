2eece569b06e63002aa768d464a3537a0f4ba9b6
cSade <83520367+cSade@users.noreply.github.com>
2021-06-18 18:32:35 -0500

    Update sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,7 +1,7 @@
-const sumAll = function(array) {
- let sum = 0;
- for (let i = 0; i < array.length; sum += array[i++]);
- return sum;
-};
+const sumAll = function(arr) {
+ return arr.reduce(function(a,b){
+  return a + b;
+ }, 0);
+}
 
 module.exports = sumAll;
