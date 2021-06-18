f22bf1b7c2e78cd1545a22a1f4731173151549f8
cSade <83520367+cSade@users.noreply.github.com>
2021-06-18 17:15:02 -0500

    Update removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
 const removeFromArray = function(array) {
   i = array.indexOf(3);
   if (i >= 0) {
-    array.splice(i, i);
+    array.splice(i, 0);
   return array
-  }
+  } else {
+    array.splice(0, i);
 };
 
 module.exports = removeFromArray;
