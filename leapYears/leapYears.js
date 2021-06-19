93d3b8584bd3667ea85a1a358f2319cebea11047
cSade <83520367+cSade@users.noreply.github.com>
2021-06-19 17:21:47 -0500

    Update leapYears.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(num) {
-  if (num%4===0) {
+  if (num%4==0 && num == 0) {
     return true;
-  } else if (num>=1) {
+  } else if (num%4==0 && num >=1) {
     return false;
   }
 };
