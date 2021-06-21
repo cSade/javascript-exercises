1149a2bf732dbaa2fe50b4869e7b1465932ae5e5
cSade <83520367+cSade@users.noreply.github.com>
2021-06-21 17:29:57 -0500

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+ let re = /[\W_]/g;
+ let strLo = str.toLowerCase().replace(re, '');
+ let reverseStr = strLo.split('').reverse().join('');
+ return reverseStr === strLo;
 };
 
 module.exports = palindromes;
