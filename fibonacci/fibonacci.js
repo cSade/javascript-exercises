e42c05bb89aed13ae6a22b0642c4e206be263d57
cSade <83520367+cSade@users.noreply.github.com>
2021-06-22 17:32:45 -0500

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  let a=0, b=1, c, s=1;
+  for (let i = 3; i<= number; i++){
+    c = a + b;
+    s = s + c;
+    a = b;
+    b = c;
+  }
 };
 
 module.exports = fibonacci;
